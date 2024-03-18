bbdbf875f29629e2ad0ca414fd148728bf3d8228
felix <fperez2020@outlook.com>
2024-03-18 16:01:16 -0400

    Remove debug print statement

--- pages/books_status.js
+++ pages/books_status.js
@@ -8,7 +8,6 @@ function get_available_books() {
 exports.show_all_books_status = async (res) => {
   try {
     let available_books = await get_available_books().exec();
-    console.log(available_books);
     res.send(available_books.map(function(b) {
       return Book(b.book).title + ' : ' + b.status;
     }));
